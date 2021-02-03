15e668f48a4407ed257b496fb3c7ceaad473042a
juan-skill <1294@holbertonschool.com>
2021-02-03 19:33:42 +0000

    [fix] Write a function that reads an entire line from a file descriptor.  return a line to  printOD

--- 0x01-getline/_getline.c
+++ 0x01-getline/_getline.c
@@ -1,79 +1,5 @@
 #include "_getline.h"
 
-/**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	char *p = s;
-
-	while (*s)
-		s++;
-	return (s - p);
-}
-
-
-/**
- * _strcat - concatenates two strings
- * @dest: copy destination
- * @src: copy source
- * Return: concatenated string
- */
-char *_strcat(char *dest, char *src)
-{
-	char *ptr = dest + _strlen(dest);
-
-	while (*src)
-		*ptr++ = *src++;
-	*ptr = 0;
-	return (dest);
-}
-
-/**
- * _strdup - returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter.
- * @str: string to be copied
- * Return: copied string
- */
-char *_strdup(char *str)
-{
-	int i, len;
-	char *copy;
-
-	if (!str)
-		return (NULL);
-	len = _strlen(str);
-	copy = malloc(sizeof(char) * len + 1);
-	if (!copy)
-	{
-		perror("Malloc failed\n");
-		exit(errno);
-	}
-	_memset(copy, '\0', len + 1);
-	for (i = 0; i < len; i++)
-		copy[i] = str[i];
-	copy[i] = 0;
-	return (copy);
-}
-
-/**
- * *_strcpy - copies the string pointed to by src,
- * including the terminating null byte
- * @dest: copied string
- * @src: string to be copied
- * Return: pointer to new copied string
- */
-char *_strcpy(char *dest, char *src)
-{
-	char *ptr = dest;
-
-	while (*src)
-		*dest++ = *src++;
-	*dest = 0;
-	return (ptr);
-}
 
 /**
  * _memset - fills the first n bytes of the memory area
@@ -105,38 +31,34 @@ void *_memset(void *arr, char c, size_t size)
  */
 char *_getline(const int fd)
 {
-	ssize_t i = 0, size = 0, t = 0, t2 = 0, n = 0;
-	char buff[READ_SIZE];
-	char *str = NULL;
+	ssize_t num_read = 0;
+	char buff[READ_SIZE], *read_line = NULL;
+	int i = 0;
 
-	while (t2 == 0 && (i = read(fd, buff, READ_SIZE - 1)))
+	read_line = (char *) malloc(sizeof(char) * (READ_SIZE + 1));
+	if (!read_line)
+		return (NULL);
+
+	_memset(read_line, '\0', sizeof(char) * (READ_SIZE + 1));
+
+	while ((num_read = read(fd, buff, BUFF_SIZE)))
 	{
-		if (i == -1 || i == 0)
-			return (NULL);
-
-		buff[i] = '\0';
-
-		n = 0;
-		while (buff[n] != '\0')
+		if (buff[0] != '\n' && buff[0] != 12)
 		{
-			if (buff[n] == '\n')
-				t2 = 1;
-			n++;
-		}
-
-		if (t == 0)
-		{
-			i++;
-			str = _strdup(buff);
-			size = i;
-			t = 1;
+			*(read_line + i) = buff[0], i++;
 		}
 		else
 		{
-			size += i;
-			str = _strcat(str, buff);
+			*(read_line + i) = '\0';
+			break;
 		}
 	}
 
-	return (str);
+	if (num_read == -1 || num_read == 0)
+	{
+		free(read_line);
+		return (NULL);
+	}
+
+	return (read_line);
 }
