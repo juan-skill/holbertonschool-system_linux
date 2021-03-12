e091f5875b012428bc77c279306035b5e207d3fb
juan-skill <1294@holbertonschool.com>
2021-03-12 04:21:29 +0000

    [fsds] 	new file:   1_main.c 	new file:   2_main.c 	modified:   Makefile

---
+++ 0x04-readelf/2_main.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of arguments of array
+ * @argv: array of arguments
+ *
+ * Return: Always (0)
+ */
+int main(int argc, char **argv)
+{
+	extern char **environ;
+	char *array_command[] = {"/usr/bin/readelf", "-W", "-l", "", NULL};
+	(void) argc;
+
+	array_command[3] = argv[1];
+	if (execve("/usr/bin/readelf", array_command, environ) == -1)
+	{
+		perror("execve");
+		return (EXIT_FAILURE);
+	}
+
+	return (EXIT_SUCCESS);
+}
