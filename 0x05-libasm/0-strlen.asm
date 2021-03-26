8fc2f7b521b364993d9f0e80a5c85a908e7bc500
juan-skill <1294@holbertonschool.com>
2021-03-26 01:59:26 +0000

    [fix] fix the counter 	modified:   0-strlen.asm

--- 0x05-libasm/0-strlen.asm
+++ 0x05-libasm/0-strlen.asm
@@ -12,7 +12,16 @@ asm_strlen:
 	push rbp		; setup
 	mov rbp, rsp		; Those two lines are equivalent to 'enter'
 
-	mov rax, 10h
+	mov rax, 0
+	jmp loop_check
+
+increase_counter:
+	add rax, 01h
+	add rdi, 01h
+
+loop_check:
+	cmp byte [rdi], 0
+	jne increase_counter
 
 	mov rsp, rbp		; Restore previous stack frame
 	pop rbp			; Those two lines are equivalent to 'leave'
