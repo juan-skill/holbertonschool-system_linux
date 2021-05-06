5e09f965911d6571e7145898dd1a4a4f6f395280
juan-skill <1294@holbertonschool.com>
2021-05-06 04:10:16 +0000

    [add] solution

---
+++ 0x07-nm_objdump/main1.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Program that reproduces the objdump command
+ * @argc: How many arguments from 
+ * @argv: The arguments array
+ * @env: The environment from shell
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *av[] = {"./hobjdump", "-sf", "", NULL};
+
+	(void)argc;
+
+	av[2] = argv[1];
+	if (execve("/usr/bin/objdump", av, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+
+	return (EXIT_SUCCESS);
+}
